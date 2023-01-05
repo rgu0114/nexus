2803bb19507acf6b3e720b20969f4821faf5b3ee
rgu0114 <richardgu0114@gmail.com>
2023-01-05 13:50:29 -0800

    Footer icon spacing and copyright

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -17,8 +17,13 @@ const useStyles = createUseStyles({
   links: {
     display: 'flex',
     justifyContent: 'space-evenly',
-    gap: '4rem',
+    gap: '2rem',
   },
+  copyright: {
+    color: '#999',
+    font: '400 0.875rem/1.5rem "Open Sans", sans-serif',
+    marginTop: '2rem'
+  }
 })
 
 export default function Contact() {
@@ -33,6 +38,10 @@ export default function Contact() {
         <SocialIcon url="https://www.facebook.com/profile.php?id=100009798823202" target="_blank" />
         <SocialIcon url="mailto:richardgu0114@gmail.com?" target="_blank" />
       </div>
+      <div className={classes.copyright}>
+        Copyright &copy; Richard Gu 2023
+      </div>
+      
     </div>
   )
 }
