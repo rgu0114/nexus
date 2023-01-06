ab887c27c6111afce8c24ce57713e2c239ca1c3e
rgu0114 <richardgu0114@gmail.com>
2023-01-05 16:25:57 -0800

    Experiences page part 2

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -7,8 +7,15 @@ const useStyles = createUseStyles({
     margin: '3rem 0 1rem 0',
     color: '#777',
   },
+  bigContainer: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    columnGap: '2rem',
+  },
   experience: {
+    display: 'flex',
     marginBottom: '2rem',
+    justifyContent: 'space-evenly'
   },
   sectionHeader: {
     display: 'flex',
@@ -17,7 +24,8 @@ const useStyles = createUseStyles({
     color: '#333',
     font: '700 2.125rem/2.75rem "Open Sans", sans-serif',
     justifyContent: 'center',
-    whiteSpace: 'nowrap'
+    whiteSpace: 'nowrap',
+    marginBottom: '4rem',
   }, 
   companyName: {
     width: 'fit-content',
@@ -41,6 +49,46 @@ const useStyles = createUseStyles({
     color: '#333',
     marginBottom: '1rem',
     marginTop: '1rem',
+  },
+  pictures: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-end'
+  },
+  logo: {
+    display: 'flex',
+    width: '240px',
+    marginTop: '2rem',
+    marginBottom: '5rem'
+  },
+  logo2: {
+    display: 'flex',
+    width: '420px',
+    aspectRatio: '439/115',
+    marginTop: '6rem',
+    marginBottom: '4rem'
+  },
+  logo3: {
+    display: 'flex',
+    width: '220px',
+    marginTop: '8rem',
+    marginBottom: '4rem'
+  },
+  description: {
+    flexDirection: 'column',
+    maxWidth: '40rem',
+    marginBottom: '4rem'
+  },
+  pairedPic: {
+    display: 'flex',
+    gap: '2rem',
+  },
+  pic: {
+    display: 'flex',
+    width: '240px',
+    aspectRatio: '548/488',
+    marginTop: '2rem',
+    marginBottom: '5rem'
   }
 })
 
@@ -50,65 +98,93 @@ const Experience = () => {
   return (
     <div className={classes.body}>
       <div className={classes.sectionHeader}> Work Experience </div>
-      <div className={classes.experience}>
-     
-        <div className={classes.companyName}>
-          Niantic
-        </div>
-        <div className={classes.role}>
-          <span style={{fontStyle: 'italic'}}> Software Engineer Intern </span> • May 2022 - August 2022
-        </div>
-          This past summer, I had an awesome experience interning at Niantic (known for Pokemon Go), as 
-          part of their 8th Wall WebAR development platform team. One of my main projects was spearheading the 
-          development of a new client onboarding page, which recommended the sample projects, tutorials, and blog posts 
-          most relevant to the user's interests, enhancing the experience for 10,000+ AR developers and boosting 
-          user activation/retention. I also revitalized the UI of the Partners page, which customers use to find verified WebAR
-          agencies to help bring their AR experiences to life, and created a localization search tool to find 
-          geographically close options. More technical details can be found in my resume. I received a return offer 
-          and will be returning as an intern for summer 2023.
-        <div className={classes.skills}>
-          Skills: TypeScript, React, Redux, JSS, Node.js, Google APIs (YouTube & Maps), AWS Elastic Beanstalk
-        </div>
-      </div>
+        <div className={classes.bigContainer}>
+          <div className={classes.pictures}> 
+            <div className={classes.pairedPic}>
+              <img
+                  className={classes.logo}
+                  src='./images/niantic.jpg'
+                  alt='Niantic blue logo'
+              />
+              <img
+                  className={classes.pic}
+                  src='./images/picwithkyle.png'
+                  alt='Pic with my 8th Wall mentor'
+              />
+            </div>
+            <img
+                className={classes.logo2}
+                src='./images/cornell_cis.png'
+                alt='Cornell CIS'
+            />
+            <img
+                className={classes.logo3}
+                src='./images/logo.svg'
+                alt='Cornell DTI'
+            />
+          </div>
+          
+          <div style={{flexDirection: 'column'}}> 
+            <div className={classes.description}>
+              <div className={classes.companyName}>
+                Niantic
+              </div>
+              <div className={classes.role}>
+                <span style={{fontStyle: 'italic'}}> Software Engineer Intern </span> • May 2022 - August 2022
+              </div>
+                This past summer, I had an awesome experience interning at Niantic (known for Pokemon Go), as 
+                part of their 8th Wall WebAR development platform team. One of my main projects was spearheading the 
+                development of a new client onboarding page, which recommended the sample projects, tutorials, and blog posts 
+                most relevant to the user's interests, enhancing the experience for 10,000+ AR developers and boosting 
+                user activation/retention. I also revitalized the UI of the Partners page, which customers use to find verified WebAR
+                agencies to help bring their AR experiences to life, and created a localization search tool to find 
+                geographically close options. More technical details can be found in my resume. I received a return offer 
+                and will be returning as an intern for summer 2023.
+              <div className={classes.skills}>
+                Skills: TypeScript, React, Redux, JSS, Node.js, Google APIs (YouTube & Maps), AWS Elastic Beanstalk
+              </div>
+            </div>
+      
+       
+            <div className={classes.description}>
+              <div className={classes.companyName}>
+                Cornell University CIS
+              </div>
+              <div className={classes.role}>
+                <span style={{fontStyle: 'italic'}}> Teaching Assistant, Data Structures & OO Programming </span> • August 2022 - Present
+              </div>
+                During the semester, I'm a Teaching Assistant/Course Consultant for Cornell's core OO programming class, which had a record 
+                size this past semester of almost 700 students. I help facilitate discussion sections of around 40 students, grade assignments/exams,
+                and hold weekly office hours to help answer students' questions about course organization or projects. 
+              <div className={classes.skills}>
+                Skills: Java, Eclipse IDE, Leadership, Public Speaking, Presentation
+              </div>
+            </div>
 
-      <div className={classes.experience}>
-        <div className={classes.companyName}>
-          Cornell University CIS
-        </div>
-        <div className={classes.role}>
-          <span style={{fontStyle: 'italic'}}> Teaching Assistant, Data Structures & OO Programming </span> • August 2022 - Present
-        </div>
-          During the semester, I'm a Teaching Assistant/Course Consultant for Cornell's core OO programming class, which had a record 
-          size this past semester of almost 700 students. I help facilitate discussion sections of around 40 students, grade assignments/exams,
-          and hold weekly office hours to help answer students' questions about course organization or projects. 
-        <div className={classes.skills}>
-          Skills: Java, Eclipse IDE, Leadership, Public Speaking, Presentation
-        </div>
-      </div>
+            <div className={classes.description}>
+              <div className={classes.companyName}>
+                Cornell Design & Tech Initiative 
+              </div>
+              <div className={classes.role}>
+                <span style={{fontStyle: 'italic'}}> Software Developer </span> • February 2022 - Present
+              </div>
+              
+                I'm a full-stack developer for Cornell DTI, an engineering project team dedicated to building out web apps and other 
+                software to enact social good on our campus community. I'm currently working on Zing, which is an app designed to optimize 
+                student grouping with nearly 1000 users from 300+ courses. We aim to save manual course staff hours spent forming groups, 
+                as well as create productive groupings with students' strengths and weaknesses taken into consideration. Some projects I've 
+                led include streamlining student group filter matching, Firebase logging of API requests for security, and generating automated 
+                email template for a variety of situations, including newly matched students.
 
+              <div className={classes.skills}>
+                Skills: TypeScript, React, Node.js, Firebase, CSS/SASS, Git
+              </div>
+            </div>
 
-      <div className={classes.experience}>
-        <div className={classes.companyName}>
-          Cornell Design & Tech Initiative 
+            <div className={classes.companyName}>
+            </div>
+
         </div>
-        <div className={classes.role}>
-          <span style={{fontStyle: 'italic'}}> Software Developer </span> • February 2022 - Present
-        </div>
-        
-          I'm a full-stack developer for Cornell DTI, an engineering project team dedicated to building out web apps and other 
-          software to enact social good on our campus community. I'm currently working on Zing, which is an app designed to optimize 
-          student grouping with nearly 1000 users from 300+ courses. We aim to save manual course staff hours spent forming groups, 
-          as well as create productive groupings with students' strengths and weaknesses taken into consideration. Some projects I've 
-          led include streamlining student group filter matching, Firebase logging of API requests for security, and generating automated 
-          email template for a variety of situations, including newly matched students.
-
-        <div className={classes.skills}>
-          Skills: TypeScript, React, Node.js, Firebase, CSS/SASS, Git
-        </div>
-
-        <div className={classes.companyName}>
-      </div>
-
 
       </div>
     </div>
